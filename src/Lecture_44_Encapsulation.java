2b741bece767c86d25ce258dc07113283c2437aa
Pradthelegend <pradeep.studies06@gmail.com>
2025-06-01 20:54:10 -0400

    Adding Lecture 44 code

---
+++ src/Lecture_44_Encapsulation.java
@@ -0,0 +1,35 @@
+class Encapsulation{
+    //By making instance variables private, you cannot access them directly from outside the class using an object like obj.name = "Hello";.
+    //They can only be accessed or modified through methods inside the same class, typically using getters and setters.
+    //This is called encapsulation — we are encapsulating (hiding) the instance variables and controlling access through the class's methods.
+    private String name;
+    private int age;
+
+    public String getName(){
+        return name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public void setName(String Name){
+        this.name= Name;
+    }
+
+    public void setAge(int Age){
+        this.age= Age;
+    }
+}
+
+public class Lecture_44_Encapsulation {
+
+    public static void main(String args[]){
+
+        Encapsulation obj = new Encapsulation();
+        obj.setName("Prad");
+        obj.setAge(22);
+        System.out.println(obj.getName());
+        System.out.println(obj.getAge());
+    }
+}
