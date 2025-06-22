3debf0980a7546b94767bc09b01bc5a45b717af6
Pradthelegend <pradeep.studies06@gmail.com>
2025-06-21 21:26:48 -0400

    Adding BufferReader and Scanner code

---
+++ src/Lecture_92_BufferReader_and_Scanner.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Lecture_92_BufferReader_and_Scanner {
+
+    public static void main(String args[]) throws IOException {
+
+        // System.out is a static final PrintStream object in the System class.
+        // println() is a method of the PrintStream class.
+        // System.out.println prints the argument and moves to the next line.
+        System.out.println("Yes");
+
+        // EXAMPLE:
+        // int num = System.in.read();
+        // This would read a single byte of data from the input (typically keyboard).
+        // It returns the ASCII value of the character entered, so it only reads one character at a time.
+        // System.out.println(num);
+
+        // System.in is an InputStream (of type InputStream)
+        // It reads bytes from the standard input (keyboard) as raw data.
+        // It cannot directly read Strings or numbers properly.
+        // So we wrap it using InputStreamReader to convert bytes to characters.
+
+        InputStreamReader in = new InputStreamReader(System.in);
+        // InputStreamReader is a bridge from byte streams to character streams.
+        // It takes an InputStream (here, System.in) and reads it as characters.
+
+        BufferedReader bf = new BufferedReader(in);
+        // BufferedReader reads text from a character-input stream efficiently.
+        // It buffers characters for efficient reading of characters, arrays, or lines.
+
+        // bf.readLine() reads a full line of input and returns it as a String.
+        // We parse this string into an integer using Integer.parseInt().
+        int num = Integer.parseInt(bf.readLine());
+        System.out.println(num); // Print the integer back to the console.
+
+        bf.close(); // Always good practice to close resources to free memory.
+
+        // BufferReader was used earlier to take inputs from the user but now we have a new way to do it i.e Scanner class.
+
+    }
+
+}
---
+++ src/Lecture_92_Scanner_Class.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Lecture_92_Scanner_Class {
+
+    public static void main(String args[]) throws IOException {
+
+        System.out.println("Enetr a number : ");
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        System.out.println(num1);
+
+    }
+
+}
+
