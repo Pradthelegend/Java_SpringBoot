71feec06cc495c3da1a4b48066b3f7398fe6b62d
Pradthelegend <pradeep.studies06@gmail.com>
2025-06-23 23:09:49 -0400

    Adding patial code conatinig errors

---
+++ src/Coding_Excercise_10.java
@@ -0,0 +1,33 @@
+//Handling Exceptions in Java: Advanced Exercise: Try-Catch, Throw, and Throws in Action
+class InvalidAgeException extends Exception{
+
+    public InvalidAgeException(String message){
+        super(message);
+    }
+}
+public class Coding_Excercise_10 {
+
+    public static void checkAge(int age) throws InvalidAgeException {
+        // TODO: Throw custom exception if age < 18, else print "Access granted"
+
+        try{
+            if(age<18){
+                throw new InvalidAgeException;
+            }else{
+                System.out.println("Access granted");
+            }
+        };
+
+    }
+
+    public static void main(String[] args) {
+        // TODO: Call checkAge(16) and checkAge(21) inside separate try-catch blocks
+
+        try {
+            checkAge(20);
+        }catch (InvalidAgeException e){
+            System.out.println("All");
+        }
+    }
+
+}
