e3f3e331e0f15ee701ef390eecb1ba65dfc0c2c8
Pradthelegend <pradeep.studies06@gmail.com>
2025-06-24 22:30:45 -0400

    Comleted the code and added the fix

--- src/Coding_Excercise_10.java
+++ src/Coding_Excercise_10.java
@@ -10,13 +10,15 @@ public class Coding_Excercise_10 {
     public static void checkAge(int age) throws InvalidAgeException {
         // TODO: Throw custom exception if age < 18, else print "Access granted"
 
-        try{
-            if(age<18){
-                throw new InvalidAgeException;
-            }else{
+        try {
+            if (age < 18) {
+                throw new InvalidAgeException("This is a InvalidAgeException");
+            } else {
                 System.out.println("Access granted");
             }
-        };
+        } finally {
+            System.out.println("Finally got executed!!");
+        }
 
     }
 
@@ -24,9 +26,9 @@ public class Coding_Excercise_10 {
         // TODO: Call checkAge(16) and checkAge(21) inside separate try-catch blocks
 
         try {
-            checkAge(20);
-        }catch (InvalidAgeException e){
-            System.out.println("All");
+            checkAge(2);
+        } catch (InvalidAgeException e) {
+            System.out.println("The catche block got executed!!");
         }
     }
 
