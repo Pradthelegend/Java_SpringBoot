e290c8c3179c0012e3cbb833c9a9bb5c4d5c2f7c
Pradthelegend <pradeep.studies06@gmail.com>
2025-06-26 22:35:27 -0400

    Made changes and fixed it. Earlier it was correct there were no errors but the code didn't meet the criteria, so fixed it

--- src/Coding_Excercise_10.java
+++ src/Coding_Excercise_10.java
@@ -1,4 +1,5 @@
-//Handling Exceptions in Java: Advanced Exercise: Try-Catch, Throw, and Throws in Action
+// TODO: Define custom exception InvalidAgeException extending Exception
+
 class InvalidAgeException extends Exception{
 
     public InvalidAgeException(String message){
@@ -10,25 +11,31 @@ public class Coding_Excercise_10 {
     public static void checkAge(int age) throws InvalidAgeException {
         // TODO: Throw custom exception if age < 18, else print "Access granted"
 
-        try {
-            if (age < 18) {
-                throw new InvalidAgeException("This is a InvalidAgeException");
-            } else {
+        try{
+            if(age<18){
+                throw new InvalidAgeException("Age must be 18 or older");
+            }else{
                 System.out.println("Access granted");
             }
         } finally {
-            System.out.println("Finally got executed!!");
+
         }
 
     }
 
+
     public static void main(String[] args) {
         // TODO: Call checkAge(16) and checkAge(21) inside separate try-catch blocks
+        try {
+            checkAge(16);
+        } catch (InvalidAgeException e) {
+            System.out.println( "Exception caught: " + e.getMessage());
+        }
 
         try {
-            checkAge(2);
+            checkAge(21);
         } catch (InvalidAgeException e) {
-            System.out.println("The catche block got executed!!");
+            System.out.println( "Exception caught: " + e.getMessage());
         }
     }
 
