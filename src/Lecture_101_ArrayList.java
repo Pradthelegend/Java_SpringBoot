13e1512dbaa78e92bdb781f610c220fd05170ea4
Pradthelegend <pradeep.studies06@gmail.com>
2025-07-02 22:38:24 -0400

    Added some code and explaination

--- src/Lecture_101_ArrayList.java
+++ src/Lecture_101_ArrayList.java
@@ -1,19 +1,44 @@
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class Lecture_101_ArrayList {
 
     public static void main(String[] args) {
 
-        Collection nums = new ArrayList();
+        // The Collection interface does not support index-based access (like get(index)).
+        // If you need to access elements by index, use the List interface instead.
+
+        // Collection<Integer> nums = new ArrayList<>();
+        // nums.add(7);
+        // nums.add(8);
+        // nums.add(9);
+        // nums.add(3);
+        // nums.add(1);
+        // nums.add("5"); // Compiles without generics, but will cause runtime ClassCastException during casting.
+
+        // When using a raw Collection (no generics), all values are treated as Object.
+        // Even if you add numbers, they are autoboxed into Integer objects and stored as Object type.
+        // You have to cast them back when retrieving.
+        // for (Object n : nums) {
+        //     int num = (Integer) n; // Manual cast required
+        //     System.out.println(num * 2);
+        // }
+
+        // Using List with generics enables index access and type safety.
+        List<Integer> nums = new ArrayList<>();
         nums.add(7);
         nums.add(8);
         nums.add(9);
         nums.add(3);
         nums.add(1);
 
-        for(Object n : nums) {
-            System.out.println(n);
+        System.out.println("The index of value 8 is: " + nums.indexOf(8));  // returns 1
+        System.out.println("Element at index 2: " + nums.get(2));           // returns 9
+
+        // Looping without the need for casting
+        for (Integer n : nums) {
+            System.out.println(n * 2); // doubles the number
         }
     }
 }
