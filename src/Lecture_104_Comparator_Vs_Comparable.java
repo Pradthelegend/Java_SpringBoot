541d9ec5fd4bd92fc40bedea7bb9478d7a6a60aa
Pradthelegend <pradeep.studies06@gmail.com>
2025-07-08 23:53:52 -0400

    Partial commit

--- src/Lecture_104_Comparator_Vs_Comparable.java
+++ src/Lecture_104_Comparator_Vs_Comparable.java
@@ -1,19 +1,41 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Lecture_104_Comparator_Vs_Comparable {
 
     public static void main(String[] args) {
 
+        Comparator<Integer> comp = new Comparator<Integer>() {//Comparator is an interface which allows us to use our own logic for sorting we just need
+                                                             // to pass the ref variable as an argument to the sort().
+            @Override
+            public int compare(Integer i, Integer j) {
+                if(i%10 > j%10){
+                    return 1;
+                }else {
+                    return -1;
+                }
+            }
+        };
+
+        Comparator<String> comp1 = new Comparator<String>() {
+            @Override
+            public int compare(String i, String j) {
+
+            }
+        };
         List<Integer> nums = new ArrayList<>();
         nums.add(31);
         nums.add(82);
         nums.add(53);
         nums.add(39);
 
-        Collections.sort(nums);//Collections is a class whch has multiple methods.
+        //sort() will sort it in ascending order.
+        Collections.sort(nums,comp);//Collections is a class whch has multiple methods.Pass comp if you want to sort using your own logic.
         System.out.println(nums);
+
+
 
 
     }
