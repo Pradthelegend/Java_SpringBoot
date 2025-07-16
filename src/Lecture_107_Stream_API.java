d804b0fa52a2f7668bddbeaaa8487509ab419467
Pradthelegend <pradeep.studies06@gmail.com>
2025-07-15 20:32:01 -0400

    Partial commit

--- src/Lecture_107_Stream_API.java
+++ src/Lecture_107_Stream_API.java
@@ -9,8 +9,11 @@ public class Lecture_107_Stream_API {
         List<Integer> nums = Arrays.asList(4, 6, 7, 3, 2, 9);
 
         Stream<Integer> s1 = nums.stream(); //The stream() will return an object of Stream interface.
+        Stream<Integer> s2 = s1.filter(n -> n%2==0);
+        Stream<Integer> s3 = s2.map(n -> n*2);
 
-        s1.forEach(n -> System.out.println(n));
+
+        s3.forEach(n -> System.out.println(n));
 
     }
 }
