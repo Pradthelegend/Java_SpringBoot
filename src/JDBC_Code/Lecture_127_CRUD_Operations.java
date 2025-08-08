7e5e1d9a295580054261a69d9ff347458bbe79b5
Pradthelegend <pradeep.studies06@gmail.com>
2025-08-07 23:12:02 -0400

    Added CRUD operation code

--- src/JDBC_Code/Lecture_127_CRUD_Operations.java
+++ src/JDBC_Code/Lecture_127_CRUD_Operations.java
@@ -12,7 +12,9 @@ public class Lecture_127_CRUD_Operations {
         String url ="jdbc:postgresql://localhost:5432/demo";
         String user = "postgres";
         String pwd = "";
-        String sql ="INSERT INTO students (name,marks) VALUES ('Bro', 99)";
+        String insertSqlQuery = "INSERT INTO students (name,marks) VALUES ('Charles', 79)";
+        String upadteQuery = "update students set name='Max' where id=5";
+        String deleteQuery = "delete from students where id=7";
 
         try {
             Class.forName("org.postgresql.Driver"); // Loading and registering the driver. Its optional.
@@ -23,9 +25,10 @@ public class Lecture_127_CRUD_Operations {
         try {
             Connection connection = DriverManager.getConnection(url,user,pwd);
             System.out.println("Connection established");
-            Statement stmnt = connection.createStatement(); //Will return a Statement object.
-            boolean status = stmnt.execute(sql);
-            System.out.println(status);
+            Statement statement = connection.createStatement(); //Will return a Statement object.
+//            statement.execute(insertSqlQuery);//You can use execute() when inserting,deleting etc. Use executeQuery() only when you are pulling data from the database.
+            //statement.execute(upadteQuery);
+            statement.execute(deleteQuery);
             connection.close();
             System.out.println("Connection Closed");
 
